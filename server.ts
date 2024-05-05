11b7c717e56f89263951559411381ed2465e2923
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2024-05-05 19:34:59 -0400

    arrangé les bloques de code pour le faire plus lisible

--- server.ts
+++ server.ts
@@ -14,7 +14,6 @@ app.use(
     cors({
         // origin: "process.env.CLIENT_URL",
         origin: "*",
-
     })
 );
 
@@ -26,34 +25,8 @@ app.get("/authorisation", (_, response: Response) => {
     const redirectURL = `https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${process.env.CLIENT_REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing`;
 
     response.json({
-        url: redirectURL
+        url: redirectURL,
     });
-})
-
-app.post("/refresh", async (request: Request, response: Response) => {
-    const refreshToken: string = request.body.refreshToken;
-    const spotifyAPI: SpotifyWebAPI = new SpotifyWebAPI({
-        redirectUri: process.env.CLIENT_REDIRECT_URI,
-        clientId: process.env.SPOTIFY_CLIENT_ID,
-        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-        refreshToken: refreshToken,
-    });
-
-    try {
-        const tokenResponse = await spotifyAPI.refreshAccessToken();
-        const data = tokenResponse.body;
-        console.log(data);
-        response.json({
-            accessToken: data.access_token,
-            expiresIn: data.expires_in,
-        });
-        
-    } catch (err) {
-        console.error("Could not refresh accesss token", err); //NOTE: haven't decided if i want to make my own error handling functionality
-    }
-    finally {
-        console.log("Done attempting access token refresh");
-    }
 });
 
 app.post("/login", async (request: Request, response: Response) => {
@@ -73,12 +46,34 @@ app.post("/login", async (request: Request, response: Response) => {
             refreshToken: data.refresh_token,
             expiresIn: data.expires_in,
         });
-    } 
-    catch (err) {
+    } catch (err) {
         console.error("Could not refresh accesss token", err);
-    }
-    finally {
+    } finally {
         console.log("Done retrieval attempt of access token");
+    }
+});
+
+app.post("/refresh", async (request: Request, response: Response) => {
+    const refreshToken: string = request.body.refreshToken;
+    const spotifyAPI: SpotifyWebAPI = new SpotifyWebAPI({
+        redirectUri: process.env.CLIENT_REDIRECT_URI,
+        clientId: process.env.SPOTIFY_CLIENT_ID,
+        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+        refreshToken: refreshToken,
+    });
+
+    try {
+        const tokenResponse = await spotifyAPI.refreshAccessToken();
+        const data = tokenResponse.body;
+        console.log(data);
+        response.json({
+            accessToken: data.access_token,
+            expiresIn: data.expires_in,
+        });
+    } catch (err) {
+        console.error("Could not refresh accesss token", err); //NOTE: haven't decided if i want to make my own error handling functionality
+    } finally {
+        console.log("Done attempting access token refresh");
     }
 });
 
